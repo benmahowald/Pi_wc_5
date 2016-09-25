af620354787bcdb655c732ca07811059b73c532a
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-25 18:00:39 -0500

    post route is adding pets

--- models/petArchetype.js
+++ models/petArchetype.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Pet = new Schema({
+var petSchema = new Schema({
   name: String,
   animal: String,
   age: Number,
   pic: String
 });
 
-var pets = mongoose.model('pets', Pet);
+var pets = mongoose.model('pets', petSchema);
 module.exports = pets;
--- routers/petRoutes.js
+++ routers/petRoutes.js
@@ -8,7 +8,7 @@ var Pets = require('../models/petArchetype.js');
 
 // get route that retrieves all pets in DB
 router.get('/all', function (req, res) {
-  console.log('Get pets hit!');
+  console.log('In get all!');
 
   Pets.find({}, function (err, pets) {
     console.log('in find');
@@ -20,5 +20,26 @@ router.get('/all', function (req, res) {
     }
   }); // end find
 }); // end get route
+
+router.post('/all', function (req, res) {
+    console.log('In post all!');
+    var data = req.body;
+    var newPet = new Pets({
+      name: data.name,
+      animal: data.animal,
+      age: data.age,
+      pic: data.pic
+    }); // end new Pet
+
+    newPet.save( function(err) {
+      if (err) {
+        console.log('save error!');
+        res.sendStatus(500);
+      } else {
+        console.log('save success!');
+        res.sendStatus(200);
+      }
+    }); // end save
+}); // end post /all route
 
 module.exports = router;
